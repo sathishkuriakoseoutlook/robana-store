fb2c6cf41d18673d870327c40dca41ba3f413b16
Sathish Kuriakose <sathish.kuriakose@gmail.com>
2025-11-13 22:14:20 +0530

    Initial commit: Robana Store Medusa project

---
+++ medusa-config.ts
@@ -0,0 +1,39 @@
+import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+
+loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+
+module.exports = defineConfig({
+  projectConfig: {
+    databaseUrl: process.env.DATABASE_URL,
+    // tell Medusa where Redis is
+    redisUrl: process.env.REDIS_URL ,
+    workerMode:process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
+    http: {
+      storeCors: process.env.STORE_CORS!,
+      adminCors: process.env.ADMIN_CORS!,
+      authCors: process.env.AUTH_CORS!,
+      jwtSecret: process.env.JWT_SECRET || "supersecret",
+      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+    }
+  },
+  admin: { 
+    disable: process.env.DISABLE_MEDUSA_ADMIN === 'true',
+    backendUrl: process.env.MEDUSA_ADMIN_BACKEND_URL || "http://localhost:9000",
+  },
+    // Configure Medusa modules to use Redis for event bus and cache
+  modules: {
+    eventBus: {
+      resolve: "@medusajs/event-bus-redis",
+      options: {
+        // package expects a connection/url to Redis
+        connection: process.env.REDIS_URL || "redis://127.0.0.1:6379",
+      },
+    },
+    cacheService: {
+      resolve: "@medusajs/cache-redis",
+      options: {
+        url: process.env.REDIS_URL || "redis://127.0.0.1:6379",
+      },
+    },
+  },
+})
